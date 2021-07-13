463295314dd1e4e9cd913226e2b7152fab08a877
karapeoples <kararpeoples@yahoo.com>
2021-07-12 23:23:50 -0500

    I fell Register and LogIn are thoroughly tested for a first run I know there are more tests but glad my mind can think them up faster then I can find documentation

--- cypress/integration/auth/authRouter.spec.js
+++ cypress/integration/auth/authRouter.spec.js
@@ -5,7 +5,7 @@ describe('Testing Test', () => {
 });
 
 describe('API is Alive', () => {
-	it('Checks the server is alive', () => {
+	it('Checks The Server is Running', () => {
 		cy.request('/');
 	});
 });
@@ -13,31 +13,54 @@ describe('API is Alive', () => {
 describe('Register~Login~Authentication', () => {
 	beforeEach(() => {
 		cy.exec('knex seed:run');
-  });
-
+	});
 	it('Registers An Admin', () => {
 		cy
 			.request('POST', '/api/welcome/register', { username: 'Test Admin', password: 'password', role: 'admin' })
-			.its('body')
 			.as('newUser')
 			.then((res) => {
-				expect(res).property('token');
-				expect(res.createdUser).property('username').to.equal('Test Admin');
-				expect(res.createdUser).property('role').to.equal('admin');
-        expect(res.roleId).property('user_id').to.be.a('number');
+				expect(res.status).equal(201);
+				expect(res.body).property('token');
+				expect(res.body.createdUser).property('username').to.equal('Test Admin');
+				expect(res.body.createdUser).property('role').to.equal('admin');
+				expect(res.body.roleId).property('user_id').to.be.a('number');
 			});
-  });
+	});
+	it('Logs In An Admin', () => {
+		cy
+			.request('POST', '/api/welcome/register', { username: 'Test Admin', password: 'password', role: 'admin' })
+			.as('newUser');
+		cy.request('POST', '/api/welcome/login', { username: 'Test Admin', password: 'password' }).then((res) => {
+			expect(res.status).equal(200);
+			expect(res.body).property('token');
+			expect(res.body.user).property('username').to.equal('Test Admin');
+			expect(res.body.user).property('role').to.equal('admin');
+			expect(res.body.roleInfo).property('user_id').to.be.a('number');
+		});
+	});
 
-	it('Registers a Consumer', () => {
+	it('Registers A Consumer', () => {
 		cy
 			.request('POST', '/api/welcome/register', { username: 'Test User', password: 'password', role: 'consumer' })
-			.its('body')
 			.as('newUser')
 			.then((res) => {
-				expect(res).property('token');
-				expect(res.createdUser).property('username').to.equal('Test User');
-				expect(res.createdUser).property('role').to.equal('consumer');
-        expect(res.roleId).property('user_id').to.be.a('number');
+				expect(res.status).equal(201);
+				expect(res.body).property('token');
+				expect(res.body.createdUser).property('username').to.equal('Test User');
+				expect(res.body.createdUser).property('role').to.equal('consumer');
+				expect(res.body.roleId).property('user_id').to.be.a('number');
 			});
+	});
+	it('Logs In A Consumer', () => {
+		cy
+			.request('POST', '/api/welcome/register', { username: 'Test User', password: 'password', role: 'consumer' })
+			.as('newUser');
+		cy.request('POST', '/api/welcome/login', { username: 'Test User', password: 'password' }).then((res) => {
+			expect(res.status).equal(200);
+			expect(res.body).property('token');
+			expect(res.body.user).property('username').to.equal('Test User');
+			expect(res.body.user).property('role').to.equal('consumer');
+			expect(res.body.roleInfo).property('user_id').to.be.a('number');
+		});
 	});
 });
