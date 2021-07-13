883336c0e57de362509707aff528cdf90ee6fef9
karapeoples <kararpeoples@yahoo.com>
2021-07-12 21:59:09 -0500

    Testing the body on a register request is passing very well no to find how to test status response

--- cypress/integration/auth/authRouter.spec.js
+++ cypress/integration/auth/authRouter.spec.js
@@ -1,11 +1,43 @@
-describe("Testing Test", () => {
-  it("Testing is working", () => {
-    expect(true).to.equal(true);
-  })
-})
+describe('Testing Test', () => {
+	it('Testing is working', () => {
+		expect(true).to.equal(true);
+	});
+});
 
 describe('API is Alive', () => {
-  it('Checks the server is alive', () => {
-    cy.request('/')
-  })
-})
+	it('Checks the server is alive', () => {
+		cy.request('/');
+	});
+});
+
+describe('Register~Login~Authentication', () => {
+	beforeEach(() => {
+		cy.exec('knex seed:run');
+  });
+
+	it('Registers An Admin', () => {
+		cy
+			.request('POST', '/api/welcome/register', { username: 'Test Admin', password: 'password', role: 'admin' })
+			.its('body')
+			.as('newUser')
+			.then((res) => {
+				expect(res).property('token');
+				expect(res.createdUser).property('username').to.equal('Test Admin');
+				expect(res.createdUser).property('role').to.equal('admin');
+        expect(res.roleId).property('user_id').to.be.a('number');
+			});
+  });
+
+	it('Registers a Consumer', () => {
+		cy
+			.request('POST', '/api/welcome/register', { username: 'Test User', password: 'password', role: 'consumer' })
+			.its('body')
+			.as('newUser')
+			.then((res) => {
+				expect(res).property('token');
+				expect(res.createdUser).property('username').to.equal('Test User');
+				expect(res.createdUser).property('role').to.equal('consumer');
+        expect(res.roleId).property('user_id').to.be.a('number');
+			});
+	});
+});
